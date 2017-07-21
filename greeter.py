5d595531e69099968ba31a22f11d4cdadbcd8628
Reuven Lazarus <rzl@google.com>
2017-07-21 10:32:20 -0400

    Added "Howdy"!

--- greeter.py
+++ greeter.py
@@ -4,7 +4,8 @@ greetings = [
   "Hello",
   "hi",
   "Good morning",
+  "Howdy",
 ]
 name = raw_input("What is your name? ")
-n = random.randint(0, 2)
+n = random.randint(0, 3)
 print greetings[n] + ", " + name + "!"
